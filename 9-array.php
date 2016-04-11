3c6e8fe8e84a9550ae79ada9f98f9409e8fa079e
Stanislav Khromov <stanislav.khromov@schibsted.se>
2016-04-12 01:19:28 +0200

    Minor fixes

--- 9-array.php
+++ 9-array.php
@@ -4,17 +4,11 @@ $a = array(5, 11, "hej");
 echo $a[0];
 //5
 
-echo "<br>";
-
 echo $a[0] + $a[1];
 //16
 
-echo "<br>";
-
 echo $a[2];
 //hej
-
-echo "<br>";
 
 echo $a[3];
 //PHP Notice:  Undefined offset: 3
