c97a5d7ee80e60369b2b865838bb89d7db8e56ec
Stanislav Khromov <stanislav+github@khromov.se>
2016-04-18 00:45:18 +0200

    last

--- 14-object.php
+++ 14-object.php
@@ -1,3 +1,19 @@
 <?php
+class Dog {
+  var $name;
+  var $age;
 
- ?>
+  function __construct($name, $age) {
+    $this->name = $name;
+    $this->age = $age;
+  }
+}
+$myDog = new Dog("Fido", 8);
+
+echo $myDog->name;
+//Fido
+
+echo $myDog->age;
+//8
+
+?>
